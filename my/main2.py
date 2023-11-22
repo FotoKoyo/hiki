09d5189199d654fce08933cc9df035a328105573
ryuk <96972763+FotoKoyo@users.noreply.github.com>
2023-11-22 11:36:43 +0300

    Delete my directory

--- my/main2.py
+++
@@ -1,9 +0,0 @@
-#import pixellib
-from pixellib.instance import instance_segmentation
-#import tensorflow
-
-
-segment_image = instance_segmentation()
-segment_image.load_model("mask_rcnn_coco.h5")
-#target_classes = segment_image.select_target_classes(person=True)
-segment_image.segmentImage("city_person2.jpeg", show_bboxes = True, output_image_name = "image_new.jpg")
